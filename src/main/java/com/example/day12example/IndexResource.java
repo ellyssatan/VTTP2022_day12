c3777e8c87a49bfe9be07729f4441f1ff9bae432
ellyssatan <ellyssa.tan1807@gmail.com>
2022-07-19 11:30:35 +0800

    added currHour

--- src/main/java/com/example/day12example/IndexResource.java
+++ src/main/java/com/example/day12example/IndexResource.java
@@ -1,5 +1,7 @@
 package com.example.day12example;
 
+import java.util.Calendar;
+import java.util.Date;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +13,12 @@ public class IndexResource {
 
     @GetMapping(produces = {"text/html"})
     public String index(Model model) {
-        return "indexresource";     // must be double quote
+        model.addAttribute("currTime", (new Date()).toString());
+        
+        Calendar cal = Calendar.getInstance();
+        model.addAttribute("currHour", cal.get(Calendar.HOUR_OF_DAY));
+
+        return "indexresource";     // must be double quote, must be same as html name
 
     }
 }
