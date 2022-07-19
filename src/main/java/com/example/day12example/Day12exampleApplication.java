412e16086618b5ba80280680311148e45b468b0f
ellyssatan <ellyssa.tan1807@gmail.com>
2022-07-19 09:57:43 +0800

    initial commit

---
+++ src/main/java/com/example/day12example/Day12exampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.day12example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day12exampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day12exampleApplication.class, args);
+	}
+
+}
